48333cdd63838435ba867894756404d20815b425
sgiffard <sgiffard@localhost.localdomain>
2018-06-20 14:20:39 +0200

    random data folds

--- prepare_data.py
+++ prepare_data.py
@@ -48,40 +48,46 @@ storms=list(set(np.transpose(data)[0])) # names of storms present in ´data´
 size_crop=25
 file_image=os.path.join('/home/sgiffard/Documents/Codes/get_data_storm_forecast/data', 'data_image_pressure_lev700.csv')
 
-# get 1/4 of the years in testing, 2/4 in training (private), 1/4 in public
-years_test=[]
-years_train=[]
-years_public=[]
-for year in range(1979,2018):
-    if (year-1979)%4==3:
-        years_test.append(year)
-    elif (year-1979)%4==1:
-        years_public.append(year)
-    else:
-        years_train.append(year)
+test_split=0.25
+public_split=0.25
+randomseed=47
+# split train-test
+num_storm = len(storms)
+indices = list(range(num_storm))
+num_test = int(np.floor(test_split * num_storm))
+num_public = int(np.floor(public_split * num_storm))
+np.random.seed(randomseed)
+np.random.shuffle(indices)
 
+train_idx, public_idx, test_idx = indices[:-num_public - num_test], \
+                                 indices[-num_public - num_test:-num_test], indices[-num_test:]
 
-data_train=[data for data in data if int(data[0][:4]) in years_train]
-data_test=[data for data in data if int(data[0][:4]) in years_test]
-data_public=[data for data in data if int(data[0][:4]) in years_public]
+list_idstorms_train = [storms[i] for i in train_idx]
+list_idstorms_public = [storms[i] for i in public_idx]
+list_idstorms_test = [storms[i] for i in test_idx]
+
+data_train=[d for d in data if d[0] in list_idstorms_train]
+data_test=[d for d in data if d[0] in list_idstorms_test]
+data_public=[d for d in data if d[0] in list_idstorms_public]
+data=[]
 
 data_folders=[data_test,data_train,data_public]
-years_group=[years_test,years_train,years_public]
+list_idsstorms=[list_idstorms_test,list_idstorms_train ,list_idstorms_public ]
 names=['test','train','public']
 
-data_folders=[data_public]
-years_group=[years_public]
-names=['public']
+# data_folders=[data_test]
+# list_idsstorms=[list_idstorms_test]
+# names=['test']
 
 # loading image data one fold at a time for memory issues
-for years,data_f,name in zip(years_group,data_folders,names):
+for data_f,name,list_id in zip(data_folders,names, list_idsstorms):
     print(name)
     data_image={}
     with open(file_image, 'r') as csvfile:
         spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
         columns_image = spamreader.__next__()
         for row in spamreader:
-            if row[0] in storms and int(row[0][:4]) in years:
+            if row[0] in list_id:
                 data_image[(row[0],row[1])]=row[2:]
     print('loading images done.')
 
@@ -92,11 +98,17 @@ for years,data_f,name in zip(years_group,data_folders,names):
             data_f[i]=data_f[i]+data_image[(data_f[i][0],data_f[i][1])]
             data_image[(data_f[i][0], data_f[i][1])]=[]
 
-    if name in ['train','test']:
-        write_csv('data/' + name + '.csv', columns + columns_image[2:], data_f)
-        print('writing data csv done.')
+
+    write_csv('data/' + name + '.csv', columns + columns_image[2:], data_f)
+    print('writing data csv done.')
+    if name in ['train', 'test']:
         data_f=[]
 
+data_test=[]
+data_train=[]
+
+#Data = pd.read_csv('/home/sgiffard/Documents/Codes/get_data_storm_forecast/data/public.csv')
+#data_public=np.array(Data)
 
 print('Separating public train and test...')
 # get some public data (to test locally) - independent from test and from train on the server.
@@ -105,22 +117,25 @@ stormids_public=list(set(np.transpose(data_public)[0]))
 stormids_public_train, stormids_public_test=train_test_split(stormids_public, test_size=0.2, random_state=57)
 data_public_test=[]
 data_public_train=[]
+
 for instant in data_public:
     if instant[0] in stormids_public_test:
         data_public_test.append(instant)
     else:
         data_public_train.append(instant)
     instant=[]
+
+data_public=[]
 write_csv('data/public_train.csv', columns+ columns_image[2:],data_public_train)
 write_csv('data/public_test.csv', columns+ columns_image[2:],data_public_test)
 print('writing public data csv done.')
 
-# copy starting kit files to <ramp_kits_dir>/<ramp_name>/data
-#copyfile(
-#    os.path.join('data', 'public_train.csv'),
-#    os.path.join(ramp_kits_dir, ramp_name, 'data', 'train.csv')
-#)
-#copyfile(
-#    os.path.join('data', 'public_test.csv'),
-#    os.path.join(ramp_kits_dir, ramp_name, 'data', 'test.csv')
-#)
+#copy starting kit files to <ramp_kits_dir>/<ramp_name>/data
+copyfile(
+   os.path.join('data', 'public_train.csv'),
+   os.path.join(ramp_kits_dir, ramp_name, 'data', 'train.csv')
+)
+copyfile(
+   os.path.join('data', 'public_test.csv'),
+   os.path.join(ramp_kits_dir, ramp_name, 'data', 'test.csv')
+)
